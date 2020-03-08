d08f7ffc30e9305e4be246f6791435a38eb20710
👉Slash <18410421@life.hkbu.edu.hk>
2020-03-08 23:04:03 +0800

    Realized the "delete" function,with th:uri method

--- src/main/java/com/slash/springboot/component/LoginHandlerInterceptor.java
+++ src/main/java/com/slash/springboot/component/LoginHandlerInterceptor.java
@@ -13,17 +13,16 @@ public class LoginHandlerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Object user = request.getSession().getAttribute("loginUser");
-//        if (user == null) {
-//            //未登录
-//            request.setAttribute("msg", "没有权限，请先登录");
-//            request.getRequestDispatcher("/index.html").forward(request, response);
-//            return false;
-//
-//        } else {
-//            //已登录
-//            return true;
-//        }
-        return true;
+        if (user == null) {
+            //未登录
+            request.setAttribute("msg", "没有权限，请先登录");
+            request.getRequestDispatcher("/index.html").forward(request, response);
+            return false;
+
+        } else {
+            //已登录
+            return true;
+        }
     }
 
     @Override
